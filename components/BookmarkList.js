fdaae7f5030f565a64f27b6830cc338f8817254e
ynakajima <yuhta.nakajima@gmail.com>
2018-01-06 14:29:29 +0900

    feat(components): RefreshControl に対応

--- components/BookmarkList.js
+++ components/BookmarkList.js
@@ -1,19 +1,32 @@
 // components/BookmarkList.js
 
 import React from 'react'
-import { Text, View, FlatList } from 'react-native'
+import { Text, View, FlatList, RefreshControl } from 'react-native'
 import BookmarkListItem from './BookmarkListItem'
 
 export default class BookmarkList extends React.Component {
   state = {
+    refreshing: true,
     items: []
   }
 
-  async componentDidMount () {
+  componentDidMount () {
+    this._fetchFeed()
+  }
+
+  _fetchFeed = async () => {
     const feed = await this.props.fetchEntry()
     const items = feed.item
     console.dir(items)
-    this.setState({ items })
+    this.setState({
+      refreshing: false,
+      items
+    })
+  }
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true })
+    this._fetchFeed()
   }
 
   _keyExtractor = (item, index) => item.link
@@ -29,6 +42,12 @@ export default class BookmarkList extends React.Component {
         extraData={this.state.items}
         keyExtractor={this._keyExtractor}
         renderItem={this._renderItem}
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh.bind(this)}
+          />
+        }
       />
     )
   }
